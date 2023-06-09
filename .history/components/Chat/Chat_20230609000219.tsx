bc0d35b3ea5d64db20d23413bb88af89f2f1a8bf
Tim Kinzer <tim.kinzer@goodrx.com>
2023-06-09 08:22:08 -0700

    Update copy and small app details

--- .history/components/Chat/Chat_20230609000219.tsx
+++ .history/components/Chat/Chat_20230609000219.tsx
@@ -352,20 +352,22 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
       {!(apiKey || serverSideApiKeyIsSet) ? (
         <div className="mx-auto flex h-full w-[300px] flex-col justify-center space-y-6 sm:w-[600px]">
           <div className="text-center text-4xl font-bold text-black dark:text-white">
-            Cheers GoodRx Hackers!
+            Welcome to Chatbot UI
           </div>
           <div className="text-center text-lg text-black dark:text-white">
             <div className="mb-8">{`Chatbot UI is an open source clone of OpenAI's ChatGPT UI.`}</div>
             <div className="mb-2 font-bold">
-              Custom Health Ai Ting
+              Important: Chatbot UI is 100% unaffiliated with OpenAI.
             </div>
           </div>
           <div className="text-center text-gray-500 dark:text-gray-400">
             <div className="mb-2">
-              HealthGPT
+              Chatbot UI allows you to plug in your API key to use this UI with
+              their API.
             </div>
             <div className="mb-2">
-             Hearty
+              It is <span className="italic">only</span> used to communicate
+              with their API.
             </div>
             <div className="mb-2">
               {t(
